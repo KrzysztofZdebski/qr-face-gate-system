d9ab3c5cd31fa2eaf7086ca6e33b33a4f415d6ee
BartoszTochowicz <tochowiczbartosz@gmail.com>
2025-12-19 12:40:30 +0100

    db and controller updates

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from .db import db
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     face_encoding = db.Column(db.Text, nullable=False)  # JSON string of face encoding
